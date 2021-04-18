71305cda4c3d6ce69c00a840d35d3d6f44d0ea69
nradkova <neliradkovaa@gmail.com>
2021-04-18 20:25:59 +0300

    OpenCourse

---
+++ ALGDynamicProgrammingExercise/03.SumWithUnlimitedAmountOfCoins/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.SumWithUnlimitedAmountOfCoins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int target = int.Parse(Console.ReadLine());
+            var sums = new int[target + 1];
+            sums[0] = 1;
+            foreach (var num in numbers)
+            {
+                for (int sum = num; sum < sums.Length; sum++)
+                {
+                    sums[sum] += sums[sum - num];
+                }
+            }
+            int count = sums[target];
+            Console.WriteLine(count);
+        }
+    }
+}
