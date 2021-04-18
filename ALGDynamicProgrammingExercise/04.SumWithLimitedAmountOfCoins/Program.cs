edae7d0cf88a4e3c5e54113fe0c98b19318ecd7c
nradkova <neliradkovaa@gmail.com>
2021-04-18 20:27:16 +0300

    OpenCourse

---
+++ ALGDynamicProgrammingExercise/04.SumWithLimitedAmountOfCoins/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.SumWithLimitedAmountOfCoins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int target = int.Parse(Console.ReadLine());
+            var sums = new Dictionary<int, int>() { { 0, 1 } };
+
+
+            foreach (var num in numbers)
+            {
+                var allsums = sums.Keys.ToArray();
+                foreach (var sum in allsums)
+                {
+                    var newSum = sum + num;
+                    if (!sums.ContainsKey(newSum))
+                    {
+                        sums.Add(newSum, 1);
+                    }
+                    else
+                    {
+                        sums[newSum]++;
+                    }
+                }
+            }
+
+            int count = sums[target];
+            Console.WriteLine(count);
+        }
+    }
+}
