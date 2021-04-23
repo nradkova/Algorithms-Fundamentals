a544963311dc7fa0d9a9603d0775c7eeae0807fe
nradkova <neliradkovaa@gmail.com>
2021-04-23 21:55:53 +0300

    OpenCourse

---
+++ ALGDynamicProgrammingLab/03.LCS/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _03.LCS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first =Console.ReadLine();
+            string second =Console.ReadLine();
+            int[,] matrix = new int[first.Length + 1, second.Length + 1];
+            for (int row = 1; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 1; col < matrix.GetLength(1); col++)
+                {
+                    if (first[row-1]==second[col-1])
+                    {
+                        matrix[row, col] = matrix[row - 1, col - 1] + 1;
+                    }
+                    else
+                    {
+                        matrix[row, col] =
+                            Math.Max(matrix[row - 1, col], matrix[row, col - 1]);
+                    }
+                }
+            }
+            Console.WriteLine(matrix[first.Length,second.Length]);
+        }
+    }
+}
