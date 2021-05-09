52d70a04621bf6fff6db782d711144083bd0c6a6
nradkova <neliradkovaa@gmail.com>
2021-05-09 23:04:09 +0300

    OpenCourse

---
+++ ALGGraphTheory,Traversal,ShortestPathsLab/01.Connected Components/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Connected_Components
+{
+    class Program
+    {
+        private static Dictionary<int, List<int>> graph;
+        private static HashSet<int> visited;
+        private static Queue<int> components;
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            graph = ReadGraph(n);
+            visited = new HashSet<int>();
+            foreach (var node in graph.Keys)
+            {
+                components = new Queue<int>();
+                DFS(node);
+                if (components.Count > 0)
+                {
+                    Console.WriteLine($"Connected component: " +
+                        $"{string.Join(" ", components)}");
+                }
+            }
+        }
+
+        private static void DFS(int node)
+        {
+            if (visited.Contains(node))
+            {
+                return;
+            }
+            visited.Add(node);
+            foreach (var child in graph[node])
+            {
+                DFS(child);
+            }
+            components.Enqueue(node);
+        }
+
+        private static Dictionary<int, List<int>> ReadGraph(int n)
+        {
+            graph = new Dictionary<int, List<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                var node = i;
+                graph.Add(node, new List<int>());
+                if (!String.IsNullOrEmpty(input))
+                {
+                    graph[node] = input.Split()
+                        .Select(int.Parse)
+                        .ToList();
+                }
+            }
+            return graph;
+        }
+    }
+}
