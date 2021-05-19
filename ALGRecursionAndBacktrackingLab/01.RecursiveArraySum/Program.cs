78ad85548f9a3f94b33b07bbbf42f2528f48e320
nradkova <neliradkovaa@gmail.com>
2021-05-20 00:05:03 +0300

    OpenCourse

---
+++ ALGRecursionAndBacktrackingLab/01.RecursiveArraySum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.RecursiveArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int sum = CalculateSum(array, 0);
+            Console.WriteLine(sum);
+        }
+
+        private static int CalculateSum(int[] array, int index)
+        {
+            if (index == array.Length)
+            {
+                return 0;
+            }
+            return array[index] + CalculateSum(array, index + 1);
+        }
+    }
+}
---
+++ ALGRecursionAndBacktrackingLab/02.RecursiveDrawing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _02.RecursiveDrawing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n =int.Parse(Console.ReadLine());
+            Draw(n);
+        }
+
+        private static void Draw(int n)
+        {
+            if (n==0)
+            {
+                return;
+            }
+            Console.WriteLine($"{new string('*',n)}");
+            Draw(n - 1);
+            Console.WriteLine($"{new string('#', n)}");
+        }
+    }
+}
