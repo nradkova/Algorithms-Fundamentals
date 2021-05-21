04a21eb66925b4baa373530afb72f375a05ce2b7
nradkova <neliradkovaa@gmail.com>
2021-05-22 00:44:38 +0300

    OpenCourse

---
+++ ALGRecursionAndBacktrackingLab/04.RecursiveFactorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04.RecursiveFactorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n =int.Parse(Console.ReadLine());
+           long f= CalculateFactorial(n);
+            Console.WriteLine(f);
+        }
+
+        private static long CalculateFactorial(int n)
+        {
+            if (n==0)
+            {
+              return  1;
+            }
+            return n * CalculateFactorial(n - 1);
+        }
+    }
+}
