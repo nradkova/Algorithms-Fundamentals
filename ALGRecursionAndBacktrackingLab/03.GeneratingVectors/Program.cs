32d2e8f9de2d11c42868a5ec922b78cf1c7d3ec9
nradkova <neliradkovaa@gmail.com>
2021-05-21 10:09:58 +0300

    OpenCourse

---
+++ ALGRecursionAndBacktrackingLab/03.GeneratingVectors/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _03.GeneratingVectors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n =int.Parse(Console.ReadLine());
+            int[] vector = new int[n];
+            GenerateVector(vector, 0);
+        }
+
+        private static void GenerateVector(int[] vector, int index)
+        {
+            if (index==vector.Length)
+            {
+                Console.WriteLine(string.Join("",vector));
+                return;
+            }
+            for (int i = 0; i <= 1; i++)
+            {
+                vector[index] = i;
+                GenerateVector(vector, index+1);
+            }
+        }
+    }
+}
