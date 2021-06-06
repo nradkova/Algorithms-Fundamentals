9625c0e9a87edd34a52de92b1a3d9ceb09409857
nradkova <neliradkovaa@gmail.com>
2021-06-06 21:08:15 +0300

    OpenCourse

---
+++ ALGRecursionAndBacktrackingLab/07.RecurciseFibonacci/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07.RecurciseFibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n>=1&&n<=49)
+            {
+            Console.WriteLine(GetFibonacci(n));
+            }
+
+        }
+
+        private static int GetFibonacci(int n)
+        {
+            if (n<=1)
+            {
+                return 1;
+            }
+           return GetFibonacci(n - 1) + GetFibonacci(n - 2);
+        }
+    }
+}
